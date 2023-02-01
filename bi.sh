91b60c803f09cf7ae6f8550ab5a87a98734af2c3
Phạm Văn Nguyên <73846954+npv2k1@users.noreply.github.com>
2023-02-01 09:22:04 +0000

    update

---
+++ bi.sh
@@ -0,0 +1,5 @@
+sudo cp -r /workspaces/substrate-dev/binaryen-version_111/bin/* /bin 
+
+sudo cp -r /workspaces/substrate-dev/binaryen-version_111/include/* /usr/include 
+
+sudo cp -r /workspaces/substrate-dev/binaryen-version_111/lib/* /lib64
