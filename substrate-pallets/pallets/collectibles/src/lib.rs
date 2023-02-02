a9d9817b5637afb1a84ef173af3cab541768b6ea
Phạm Văn Nguyên <73846954+npv2k1@users.noreply.github.com>
2023-02-02 06:52:40 +0000

    update

--- substrate-pallets/pallets/collectibles/src/lib.rs
+++ substrate-pallets/pallets/collectibles/src/lib.rs
@@ -4,40 +4,54 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{
-		pallet_prelude::*,
-		traits::{Currency, Randomness},
-	};
+	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
+	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		type Currency: Currency<Self::AccountId>;
-		type CollectionRandomness: Randomness<Self::Hash, Self::BlockNumber>;
+	pub trait Config: frame_system::Config {}
 
-		#[pallet::constant]
-		type MaximumOwned: Get<u32>;
-	}
-	type BalanceOf<T> =
-		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+	#[pallet::storage]
+	#[pallet::getter(fn something)]
 
-	pub enum Color {
-		Red,
-		Yellow,
-		Blue,
-		Green,
-	}
+	pub type Something<T> = StorageValue<_, u32>;
 
-	pub struct Collectible<T: Config> {
-		// Unsigned integers of 16 bytes to represent a unique identifier
-		pub unique_id: [u8; 16],
-		// `None` assumes not for sale
-		pub price: Option<BalanceOf<T>>,
-		pub color: Color,
-		pub owner: T::AccountId,
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/main-docs/build/events-errors/
+	// #[pallet::event]
+	// #[pallet::generate_deposit(pub(super) fn deposit_event)]
+	// pub enum Event<T: Config> {}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {}
+
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// An example dispatchable that takes a singles value as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::call_index(0)]
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/main-docs/build/origins/
+			let who = ensure_signed(origin)?;
+
+			// Update storage.
+			<Something<T>>::put(something);
+
+			// Emit an event.
+			// Self::deposit_event(Event::SomethingStored { something, who });
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
 	}
 }
