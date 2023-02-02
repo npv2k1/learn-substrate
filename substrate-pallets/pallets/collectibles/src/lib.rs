b2c83e6cbc49ecfff8d85e78db973770855e40a4
Phạm Văn Nguyên <73846954+npv2k1@users.noreply.github.com>
2023-02-02 03:18:49 +0000

    frame_support

--- substrate-pallets/pallets/collectibles/src/lib.rs
+++ substrate-pallets/pallets/collectibles/src/lib.rs
@@ -4,14 +4,22 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::pallet_prelude::*;
-    use frame_system::pallet_prelude::*;
+	use frame_support::{
+		pallet_prelude::*,
+		traits::{Currency, Randomness},
+	};
+	use frame_system::pallet_prelude::*;
 
-    #[pallet::pallet]
-    #[pallet::generate_store(pub(super) trait Store)]
-    pub struct Pallet<T>(_);
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
 
-    #[pallet::config]
-    pub trait Config: frame_system::Config {
-    }
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Currency: Currency<Self::AccountId>;
+		type CollectionRandomness: Randomness<Self::Hash, Self::BlockNumber>;
+
+		#[pallet::constant]
+		type MaximumOwned: Get<u32>;
+	}
 }
